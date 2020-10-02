1bc793e6131ecd9e803cc71319bf6dd23924197f
Archana <archana.radhakrishnan@flytxt.com>
2020-10-02 14:38:35 +0530

    Final edits

--- site/script.js
+++ site/script.js
@@ -1,18 +1,33 @@
+/** 
+ * Variable storing current active carousel slide.
+ */
 var currentPos = 0;
 
+/**
+ * Function to close Covid-19 banner at the top.
+ */
 function closeCovid() {
 	document.querySelector('.covid-ad').hidden = true;
 }
 
+/** 
+ * Function to show the dropdown menu for Navigation menu option on mouseenter event.
+ */
 function showMenu(menuClass) {
 	document.getElementsByClassName(menuClass)[0].style.display = 'block';
 }
 
+/** 
+ * Function to close the dropdown menu for Navigation menu option on mouseleave event.
+ */
 function closeMenu(menuClass) {
 	document.getElementsByClassName(menuClass)[0].style.display = '';
 }
 
-function modifyLinks() {
+/** 
+ * Function to change colours of Navigation menu options depending on whih one is in focus.
+ */
+function toggleColour() {
 	let currentElement = event.currentTarget.innerText;
 	let allLinks = document.getElementsByClassName('link');
 	let allLinksArr = Array.from(allLinks);
@@ -28,6 +43,9 @@ function modifyLinks() {
 	});
 }
 
+/** 
+ * Function to set default colour for all Navigation menu options.
+ */
 function setDefaults() {
 	let allLinks = document.getElementsByClassName('link');
 	let allLinksArr = Array.from(allLinks);
@@ -38,6 +56,9 @@ function setDefaults() {
 	});
 }
 
+/** 
+ * Function to toggle accordion options, in 'Our values' section, on click.
+ */
 function toggle(option) {
 	let collapseItems = document.getElementsByClassName('collapse');
 	let collapseArr = Array.from(collapseItems);
@@ -63,6 +84,9 @@ function toggle(option) {
 	});
 }
 
+/** 
+ * Function to show the current active carousel slide.
+ */
 function setActiveSlide(action) {
 	let slides = document.getElementsByClassName('carousel-item');
 	let slidesArr = Array.from(slides);
@@ -83,6 +107,9 @@ function setActiveSlide(action) {
 	});
 }
 
+/** 
+ * Function to shift to the next carousel slide.
+ */
 function shiftNext() {
 	if (this.currentPos === 3) {
 		this.currentPos = 0;
@@ -92,6 +119,9 @@ function shiftNext() {
 	this.setActiveSlide('next');
 }
 
+/** 
+ * Function to shift to the previous carousel slide.
+ */
 function shiftPrev() {
 	if (this.currentPos === 0) {
 		this.currentPos = 3;
@@ -101,25 +131,36 @@ function shiftPrev() {
 	this.setActiveSlide('prev');
 }
 
+/** 
+ * Function to open overlay menu on clicking the hamburger icon.
+ */
 function openNav() {
 	document.getElementById("mini-menu").style.width = "100%";
 	document.body.style.overflow = 'hidden';
 	this.goBack('about');
 	this.goBack('patients');
 }
-  
+
+/** 
+ * Function to close overlay menu on clicking the close icon.
+ */
 function closeNav() {
 	document.body.style.overflow = 'auto';
 	document.getElementById("mini-menu").style.width = "0%";
 }
 
+/** 
+ * Function to show sub-menu option on clicking an option in the overlay menu, if applicable.
+ */
 function moveToSubmenu(menu) {
 	document.querySelector('.' + menu).style.display = 'block';
 	document.querySelector('.main-menu').hidden = true;
 }
 
+/** 
+ * Function to go back to main overlay menu on clicking Back option in sub-menu.
+ */
 function goBack(menu) {
 	document.querySelector('.' + menu).style.display = '';
 	document.querySelector('.main-menu').hidden = false;
 }
-  
